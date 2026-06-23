9288e6287cd011ea007a4f666ab4f808c76c1f8c
Aditya Tomar <adityatomar80050@gmail.com>
2026-06-23 22:22:43 +0530

    Containership consturcot destructor order

---
+++ advanced_feature/containership/constructor-destructor-order.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+class Engine {
+
+    public:
+
+        void startEngine(){ std::cout << "Engine has started " << std::endl; }
+
+        Engine(){ std::cout << "Engine contructor" << std::endl; }
+
+        ~Engine(){ std::cout << "Engine destructor" << std::endl; }
+
+};
+
+class Car {
+
+    private:
+        Engine engine; // CONTAINERSHIP ( CAR HAS A ENGINE )
+
+    public:
+        std::string name;
+
+        Car(std::string carName){
+            std::cout << "Car contructor" << std::endl;
+            name = carName;
+        }
+
+        ~Car(){ std::cout << "Car contructor" << std::endl; }
+
+        void displayName(void) { std::cout << "Car = " << name << std::endl ; }
+
+        void startCar(void) { engine.startEngine(); }
+};
+
+int main(){
+
+    Car alto("Alto");
+
+    alto.displayName();
+    alto.startCar();
+
+    return 0;
+}
+
+// IF WE WOULD HAVE BEEN USING INHERITANCE WE WOULD SAY CAR IS AN ENGINE WHICH IS WRONG
+
+// A famous OOP principle says:
+    // Favor composition over inheritance.
+
+// Because composition is often more flexible.
